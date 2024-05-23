01d0151a9dd5c34ceb12e5ef38c00f3217e72433
vineas <alvienasyandika@gmail.com>
2024-05-24 00:05:22 +0700

    First Commit

---
+++ java 01 - Memahami Program/OtongSurotong.java
@@ -0,0 +1,5 @@
+public class OtongSurotong {
+    public static void main (String[] args){ //
+        System.out.print("Anjay si Otong");
+    }
+}
---
+++ java 02 - Organisasi Program dengan Package/src/programku/OtongSurotong.java
@@ -0,0 +1,7 @@
+package programku;
+
+public class OtongSurotong {
+    public static void main (String[] args){ //
+        System.out.print("Anjay si Otong");
+    }
+}
